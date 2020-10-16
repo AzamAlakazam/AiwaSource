be559c4bcfe022d970d4f89aa4ea073556054cae
AzamAlakazam <68610584+AzamAlakazam@users.noreply.github.com>
2020-10-16 21:29:44 +0530

    Idfk what am i doing rn

---
+++ Assets/js/product_init.js
@@ -0,0 +1,8 @@
+<div class="topprod">
+                <section class="The_Product">
+                    <div class="new"></div>
+                    <a href="Assets/products/FAFP0954.JPG" target="_blank"><img src="Assets/products/FAFP0954.JPG" width="1280" height="1280"></a>
+                    <div class="des">AMS ColaCaps w/Vitamin C Capsules 60S<br>AED 285.00/1+1</div>
+                    <a class="ord" href="https://wa.me/971558525262?text=Hi!%20I%20Would%20Like%20To%20Purchase%20AMS%20ColaCaps%20With%20Vitamin%20C%20Capsules%2060%201%2B1" target="_blank">Order Now</a>
+                </section>
+            </div>
